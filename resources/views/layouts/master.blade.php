e009af0130d85971b7295ef2677f3f8422f5f5b8
Frank McCoy <frankmccoy.d@gmail.com>
2018-04-10 20:37:17 -0500

    assets

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,22 +8,12 @@
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
   <meta name="description" content="">
   <meta name="author" content="">
-  <link rel="icon" href="../../favicon.ico">
+  <link rel="icon" href="{{ asset('favicon.ico') }}">
 
   <title>Starter Template for Bootstrap</title>
 
   <!-- App Core CSS -->
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-  <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="starter-template.css" rel="stylesheet">
-
-  <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-  <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-  <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
 
   <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
   <!--[if lt IE 9]>
@@ -43,31 +33,23 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
+        <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name') }}</a>
       </div>
       <div id="navbar" class="collapse navbar-collapse">
-        {!! $primarynav->asUl() !!}
+        {!! $primarynav->asUl(['class' => 'nav navbar-nav']) !!}
       </div><!--/.nav-collapse -->
     </div>
   </nav>
 
-  <div class="container-fluid">
-
-    <div class="starter-template">
-      <h1>Bootstrap starter template</h1>
-      <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+  <div id="app" class="container-fluid">
+    <div class="row">
+      @section('body')
+        @yield('content')
+      @show
     </div>
-
   </div><!-- /.container -->
 
-
-  <!-- Bootstrap core JavaScript
-  ================================================== -->
-  <!-- Placed at the end of the document so the pages load faster -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-  <script src="../../dist/js/bootstrap.min.js"></script>
-  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-  <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+  <!--App Core JavaScript -->
+  <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
